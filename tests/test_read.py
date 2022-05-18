96e5adcdf6c21668699036d029089a534a42f6fb
Stephan Malzacher <48381090+StephanM87@users.noreply.github.com>
2022-05-18 09:36:30 +0200

    feat: omex updated

--- tests/test_read.py
+++ tests/test_read.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 def test_request():
-    files = {"omex_archieve":open("tests/enzml.omex", "rb")}
+    files = {"omex_archive":open("tests/enzml.omex", "rb")}
 
     req = requests.post("http://127.0.0.1:5000/read", files=files)
     
-    print(req.text)
+    print("RESULT",req)
+    print("RESULT",req.text)
